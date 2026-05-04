edb5c241efccdd9db77ef32db19aa67765cf2e26
msVill <msv.ltmc@gmail.com>
2026-05-03 19:43:19 -0700

    Step 2 complete (double checked in case i couldn't use the same values either)

--- 210-lab-34.cpp
+++ 210-lab-34.cpp
@@ -6,7 +6,7 @@
 #include <limits.h> // Dijkstra's algorithm.
 using namespace std;
 
-const int SIZE = 7;
+const int SIZE = 9; //updated
 
 struct Edge {
     int src, dest, weight;
@@ -90,9 +90,11 @@ class Graph {
 int main() {
 
     vector<Edge> edges = {
-        {0,1,12}, {0,2,8}, {0,3,21},
-        {2,3,6}, {2,6,2}, {2,4,4}, {2,5,5},
-        {5,6,6}, {4,5,9}
+        {0,1,8}, {0,2,21}, {1,2,6},
+        {1,3,5}, {1,4,4}, {2,7,11},
+        {2,8,8}, {3,4,9}, {5,6,10},
+        {5,7,15}, {5,8,5}, {6,7,3},
+        {6,8,7}
     };
 
     Graph g(edges);
